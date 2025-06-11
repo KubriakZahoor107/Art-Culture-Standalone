383947fc58a9a3a61194947eebd8b8bef9c065ee
Kostiantyn Kubriak <konstantin.kubriak@gmail.com>
2025-06-11 12:08:05 +0300

    WIP: my local fixes before merging Codex changes

--- jest.config.ts
+++ jest.config.ts
@@ -1,34 +1,20 @@
-import type { Config } from 'jest'
+iimport type { Config } from 'jest'
 
 const config: Config = {
-  // Використовує Babel для трансформації .ts/.tsx/.js/.jsx
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-
-  // Підтримка ESM для цих розширень (без явного ".js")
-  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
-
-  // Polyfill для TextEncoder/TextDecoder
-  setupFiles: ['<rootDir>/jest.setup.cjs'],
-
-  testEnvironment: 'jsdom',
-
-  // Мапінг псевдоніма @/ до папки src/
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|svg|ttf|webp)$': '<rootDir>/__mocks__/fileMock.js',
-  },
-
-  // Розширення файлів, які обробляє Jest
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-
-  // Шляхи, які ігноруються при тестуванні
-  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/dist/'],
-
-  // Ігнорувати перетворення у node_modules
-  transformIgnorePatterns: ['/node_modules/'],
+    transform: {
+        '^.+\\.[tj]sx?$': 'babel-jest',
+    },
+    extensionsToTreatAsEsm: ['.ts', 'tsx', 'jsx'],
+    setupFiles: ['<rootDir>/jest.setup.cjs'],
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '\\.(jpg|jpeg|png|svg|ttf|webp)$': '<rootDir>/__mocks__/fileMock.js',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testPathIgnorePatterns: ['/node_modules/', '/.next/', '/dist/'],
+    transformIgnorePatterns: ['/node_modules/'],
 }
 
 export default config
