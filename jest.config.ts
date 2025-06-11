32e9c6bf15a8a8291f698bf4ab16f1de2d345c84
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-06-11 12:08:36 +0300

    Fix Jest/Babel setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,20 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest'
+  },
   extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
+  setupFiles: ['<rootDir>/jest.setup.cjs'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|svg|ttf|webp)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|svg|ttf|webp)$': '<rootDir>/__mocks__/fileMock.js'
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/dist/'],
-  transformIgnorePatterns: ['/node_modules/'],
+  moduleFileExtensions: ['ts','tsx','js','jsx','json','node'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/','/.next/','/dist/'],
+  transformIgnorePatterns: ['/node_modules/']
 }
 
 export default config
