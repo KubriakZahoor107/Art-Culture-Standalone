618b0f79e174d0fce9798ff0845bb39474803664
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-06-16 00:47:10 +0300

    Stage 0: automated CI fixes by Codex

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,18 +1,4 @@
-/** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@components/(.*)$': '<rootDir>/src/assets/components/$1',
-    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|svg|ttf|webp)$': '<rootDir>/__mocks__/fileMock.js',
-  },
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
 };
